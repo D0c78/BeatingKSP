e3f49ebd8677c44635f50e7be6acdcb517fc49e7
Davide Farris <davide.farris@skywarder.eu>
2025-08-30 12:32:30 +0200

    First (serious) commit

---
+++ utils.py
@@ -0,0 +1,34 @@
+import numpy as np
+import ussa1976 as isa
+
+# Class to create rocket data
+class rocket: 
+    def __init__(self, 
+                 I_sp: float = 300,
+                 m_p: float = 100,
+                 A: float = 0,
+                 p_e: float = 0,
+                 C_D0: float = 0.5,
+                 S: float = 100):
+        rocket.I_sp = I_sp
+        rocket.m_p = m_p
+        rocket.A = A
+        rocket.p_e = p_e
+        rocket.C_D0 = C_D0
+        rocket.S = S
+
+# Class to create environment data
+class env:
+    def __init__(self,
+                 ):
+        env.p: float = lambda h : isa.compute(h, variables=['p'])
+        env.rho: float = lambda h : isa.compute(h, variables=['rho'])
+
+# Define propagator
+class propagator:
+    def __init__(self, funcSim):
+        propagator.dy = funcSim
+
+
+
+    
